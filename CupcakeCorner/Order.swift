50ec640dfa81b7dc4a6b7724d8f128d5adfd1ff7
Armando Francisco <armando17171771@gmail.com>
2024-11-17 10:45:16 -0500

    Preparing for checkout

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -14,12 +14,40 @@ class Order {
     var type = 0
     var quantity = 3
     
+    var extraFrosting = false
+    var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
     var hasValidAddress: Bool {
         if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
             return false
         }
 
         return true
+    }
+    
+    var cost: Decimal {
+        // $2 per cake
+        var cost = Decimal(quantity) * 2
+
+        // complicated cakes cost more
+        cost += Decimal(type) / 2
+
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+
+        return cost
     }
 
     var specialRequestEnabled = false {
@@ -30,11 +58,4 @@ class Order {
             }
         }
     }
-    var extraFrosting = false
-    var addSprinkles = false
-    
-    var name = ""
-    var streetAddress = ""
-    var city = ""
-    var zip = ""
 }
